5da751a8d8730a19ffd3fc96d67db28aca7088d1
hi117 <shizuokatea314@yahoo.co.jp>
2024-08-16 15:40:27 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'plaster_pastedeploy',
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'alembic',
+    'pyramid_retry',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+]
+
+tests_require = [
+    'WebTest',
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='memo_python_pyramid',
+    version='0.0',
+    description='memo_python_pyramid',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = memo_python_pyramid:main',
+        ],
+        'console_scripts': [
+            'initialize_memo_python_pyramid_db=memo_python_pyramid.scripts.initialize_db:main',
+        ],
+    },
+)
